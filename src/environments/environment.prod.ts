16d1cf9a53fba97840d6214175d364c13a08da2b
JoelFX <98891378+JoelCruzAldecua@users.noreply.github.com>
2022-10-19 11:16:02 -0500

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://crm-fx.herokuapp.com/',
-  /* pruebas api */
-  //api: 'https://fibrax-testing.herokuapp.com/',
-  dominio: 'https://fibraxinversiones.mx/',
+  api: 'https://apirainsforest.herokuapp.com/',
+  dominio: 'https://rainforest-dashboard.netlify.app/',
 };
